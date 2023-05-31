c5ef5163c18a41b56b6cbd96328b8d4031a3afff
Shin Dain <codedain@gmail.com>
2023-05-31 09:36:49 +0900

    feat: axios request, response 공통화처리
    baseURL설정 및 에러처리

---
+++ src/components/WishHistory.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { wishHistoryList } from "@/services/requestHandler";
+import { ref, onMounted } from "vue";
+
+const result = ref({});
+onMounted(() => {
+  wishHistoryList().then((response) => {
+    result.value = response;
+    console.log(result);
+  });
+});
+</script>
+
+<template></template>
+
+<style scoped></style>
---
+++ src/views/WishHistoryView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import WishHistory from "@/components/WishHistory.vue";
+</script>
+
+<template>
+  <WishHistory></WishHistory>
+</template>
+<style scoped></style>
