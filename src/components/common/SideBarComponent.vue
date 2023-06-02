c86d04b810c77a4efa31e6ec70194954b0fcd5f0
choijungwoo493 <jwoo1016@gmail.com>
2023-06-02 17:46:04 +0900

    관리자 페이지의 회원관리 기능 구현

--- src/components/common/SideBarComponent.vue
+++ src/components/common/SideBarComponent.vue
@@ -9,7 +9,7 @@
         <ul class="space-y-2 font-medium">
           <li>
             <router-link
-              to="/memberManagement"
+              to="/ memberManagement"
               class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
             >
               <svg
