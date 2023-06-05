2077a2d319e7c9e69084d7fa94876ff8acd65b88
Shin Dain <codedain@gmail.com>
2023-06-05 19:51:05 +0900

    feat: user wish main css수정

--- src/components/common/UserWishComponent.vue
+++ src/components/common/UserWishComponent.vue
@@ -1,34 +1,34 @@
-<template class="w-full">
-  <div class="bg-white shadow border border-gray-300">
-    <div class="w-full flex">
-      <a href="/wishHistory" class="h-48 overflow-hidden block">
-        <img
-          class="w-full"
-          src="../../../public/images/default.png"
-          alt="default"
-        />
-      </a>
-    </div>
-    <div class="px-1.5">
-      <h5
-        class="my-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white"
-      >
-        {{ props.title }}
-      </h5>
-      <a href="/">
-        <h5
-          class="my-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white"
+<template>
+  <div class="w-1/3 p-6">
+    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+      <div class="rounded-lg w-full">
+        <a
+          href="/wishHistory"
+          class="relative w-full overflow-hidden bg-cover bg-no-repeat h-96"
         >
-          {{ props.productName }}
+          <img
+            class="w-full transition duration-300 ease-in-out hover:scale-110"
+            src="../../../public/images/default.png"
+            alt="default"
+          />
+        </a>
+      </div>
+      <div class="px-6 pb-10 pt-12">
+        <h5 class="font-bold text-gray-600 text-lg mb-4">
+          {{ props.title }}
         </h5>
-      </a>
-
-      <p class="mb-1.5 font-normal text-gray-700 dark:text-gray-400">
-        목표금액 :
-        <span class="text-blue-700 font-bold"
-          >{{ formatAmount(props.goalAmount) }} 원</span
-        >
-      </p>
+        <a href="/">
+          <h5 class="text-gray-500 text-sm">
+            {{ props.productName }}
+          </h5>
+        </a>
+        <p class="text-gray-500 text-sm">
+          목표금액 :
+          <span class="text-blue-700 font-bold"
+            >{{ formatAmount(props.goalAmount) }} 원</span
+          >
+        </p>
+      </div>
     </div>
   </div>
 </template>
--- src/views/wish/UserWishMainView.vue
+++ src/views/wish/UserWishMainView.vue
@@ -1,32 +1,34 @@
 <template class="max-w-screen-lg">
-  <div
-    class="max-w-screen-lg text-center mx-auto py-14 sm:pt-22 sm:pb-10 sm:py-48 lg:pt-30 lg:pb-10"
-  >
-    <!--    <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">-->
-    <!--      OOO 님의 위시 리스트에요.🧞‍♂️-->
-    <!--    </h1>-->
-  </div>
-  <div class="w-full flex">
-    <div class="w-full flex flex-row">
-      <UserWishComponent
-        class="w-1/3"
-        v-for="(wish, index) in wishList"
-        :key="index"
-        :wishId="wish.id"
-        :userId="wish.userId"
-        :title="wish.title"
-        :content="wish.content"
-        :imgage="wish.image"
-        :productName="wish.productName"
-        :goalAmount="wish.goalAmount"
-        :isPublic="wish.isPublic"
-        :completionStatus="wish.completionStatus"
-        :registerDatetime="wish.registerDatetime"
-        :modifyDatetime="wish.modifyDatetime"
-      />
+  <div class="bg-gray-50">
+    <div
+      class="max-w-screen-lg text-center mx-auto py-14 sm:pt-22 sm:pb-10 sm:py-48 lg:pt-30 lg:pb-10"
+    >
+      <!--    <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">-->
+      <!--      OOO 님의 위시 리스트에요.🧞‍♂️-->
+      <!--    </h1>-->
+    </div>
+    <div class="m-auto" style="max-width: 1024px">
+      <div class="w-full m-auto flex flex-wrap">
+        <UserWishComponent
+          v-for="(wish, index) in wishList"
+          :key="index"
+          :wishId="wish.id"
+          :userId="wish.userId"
+          :title="wish.title"
+          :content="wish.content"
+          :imgage="wish.image"
+          :productName="wish.productName"
+          :goalAmount="wish.goalAmount"
+          :isPublic="wish.isPublic"
+          :completionStatus="wish.completionStatus"
+          :registerDatetime="wish.registerDatetime"
+          :modifyDatetime="wish.modifyDatetime"
+        />
+      </div>
     </div>
   </div>
 </template>
+
 <script setup>
 import UserWishComponent from "@/components/common/UserWishComponent.vue";
 import { ref } from "vue";
